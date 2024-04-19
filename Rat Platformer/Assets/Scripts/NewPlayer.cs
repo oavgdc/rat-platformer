d0b98a53f2e0ad4464f94ecb497dfc8fb619a827
oavgdc <114132528+YouAteMyPie@users.noreply.github.com>
2024-04-18 18:38:57 -0700

    UI (Part 2 - Health)

--- Rat Platformer/Assets/Scripts/NewPlayer.cs
+++ Rat Platformer/Assets/Scripts/NewPlayer.cs
@@ -11,15 +11,20 @@ public class NewPlayer : PhysicsObject
     [SerializeField] private float jumpPower = 10;
 
     public int coinsCollected = 0;
-
-    public int health = 6;
-    public Image[] hearts;
-
     public Text coinsText;
+
+    public float health = 3f;
+    public Image[] hearts;
+    [SerializeField] public Sprite heartFull;
+    [SerializeField] public Sprite heartHalf;
+    [SerializeField] public Sprite heartEmpty;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        UpdateUI();
         //Note: since coinsText UI element is only every gonna be used one time, we can just assign reference
         //in unity inspector by dragging and dropping onto text field (in other words, below line is unnecessary)
         //below line would be good for multiple objects referencing the same thing 
@@ -43,7 +48,22 @@ public class NewPlayer : PhysicsObject
     public void UpdateUI() 
     {
         coinsText.text = coinsCollected.ToString() + " Coins";
-
+        for (int i = 0; i < hearts.Length; i++) 
+        {
+            if (health >= i + 1)
+            {
+                hearts[i].sprite = heartFull;
+            }
+            else if (i < health && health < i + 1)
+            {
+                hearts[i].sprite = heartHalf;
+            }
+            else
+            {
+                hearts[i].sprite = heartEmpty;
+            }
+        
+        }
     } 
 
 }
