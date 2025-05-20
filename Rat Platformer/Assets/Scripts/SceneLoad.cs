fe8c9794985a9f29b4037c3f004cf360087bf3b9
Joseph Lee <114132528+YouAteMyPie@users.noreply.github.com>
2025-05-20 04:43:00 -0700

    uhbyyuihhyuyuh

---
+++ Rat Platformer/Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    public GameObject trigger;
+    public GameObject player;
+    public Animator transition;
+    public Vector2 whereToSpawnTo;
+    public VectorValue playerStorage;
+
+    public float transitionTime = 1.0f;
+
+    public string sceneToLoadName;
+    
+    void Start()
+    {
+        
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.tag == "Player" && !collider.isTrigger)
+        {
+            StartCoroutine(LoadLevel());
+        }
+    }
+
+    public IEnumerator LoadLevel()
+    {
+        playerStorage.pos = whereToSpawnTo;
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneToLoadName);
+
+    }
+}
+
---
+++ Rat Platformer/Assets/Scripts/VectorValue.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class VectorValue : ScriptableObject
+{
+
+    public Vector2 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
